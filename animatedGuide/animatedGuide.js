33494f201498ba8435b8ffe5cd12d6f1d43b7075
Ari S <echomanari@gmail.com>
2017-05-26 07:12:00 -0700

    Update animatedGuide.js

--- animatedGuide/animatedGuide.js
+++ animatedGuide/animatedGuide.js
@@ -4,11 +4,13 @@
     $('.slide').css({transform:"translate(100vw,0)"});
     $('.slide[data-slidenum=\'1\'').css({translate:[0,0]})
     var resolveSlide = function(newSlide) {
+        if($('.slide[data-slidenum=\'' + newSlide + '\']').length !== 0)) {
             $('.slide[data-slidenum=\'' + selectedSlide + '\']').transit({translate:["-100vw",0]}, function() {
                 $(this).css({transform:"translate(100vw,0)"});
             });
             $('.slide[data-slidenum=\'' + newSlide + '\']').transit({translate:[0,0]});
             selectedSlide = newSlide;
+        }
     };
     $('html').mouseup(function(e) {
             resolveSlide(selectedSlide + 1);
